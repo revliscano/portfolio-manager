cdf7aa57aafc8fac4a33a69f8205f14c7e688fa1
revliscano <revliscano@gmail.com>
2021-02-03 00:01:34 -0400

    Cleaned tearDown() method for Technology Model
    Added a test to verify that no repeated pairs are allowed in the m2m relationship between Project and Technology

--- apps/portfolio/tests/test_models.py
+++ apps/portfolio/tests/test_models.py
@@ -1,14 +1,20 @@
 import re
 import os
 
+from functools import partial
+
 from django.test import TestCase
 from django.conf import settings
+from django.db import IntegrityError
+
 from mixer.backend.django import mixer
 
 from apps.portfolio.models import Project, Technology
 
 
 class TestProjectModel(TestCase):
+    EMPTY_LIST = []
+
     def test_project_is_saved(self):
         given_project = mixer.blend(Project, name="test")
         fetched_project = Project.objects.get(name="test")
@@ -18,13 +24,33 @@ class TestProjectModel(TestCase):
             msg='Should create a new object'
         )
 
+    def test_project_may_have_no_technologies(self):
+        given_project_data = dict(
+            name='some name',
+            description='some description',
+            year=2020
+        )
+        created_project = Project.objects.create(**given_project_data)
+
+        self.assertEqual(
+            list(created_project.technologies.all()),
+            self.EMPTY_LIST,
+            msg='Project objects should might have no technologies associated'
+        )
+
 
 class TestTechnologyModel(TestCase):
+    FIRST_ONE = 0
+
+    def setUp(self):
+        self.tear_down_assistant = TestImagesEraser()
+        self.five_different_technologies = mixer.cycle(5).blend(Technology)
+
     def test_project_has_multiple_techs(self):
-        five_technologies_used = mixer.cycle(5).blend(Technology)
+        expected_technologies_count = 5
         given_project = mixer.blend(
             Project,
-            technologies=five_technologies_used
+            technologies=self.five_different_technologies
         )
 
         project_technologies_count = Technology.objects.filter(
@@ -33,14 +59,33 @@ class TestTechnologyModel(TestCase):
 
         self.assertEqual(
             project_technologies_count,
-            5,
+            expected_technologies_count,
             msg='Project should have 5 technologies'
         )
 
+    def test_project_and_tech_pair_cannot_be_repeated(self):
+        given_technology = self.five_different_technologies[self.FIRST_ONE]
+        given_project = mixer.blend(
+            Project,
+            technologies=[given_technology]
+        )
+
+        given_project.technologies.add(given_technology)
+
+        self.assertEqual(
+            list(given_project.technologies.all()),
+            [given_technology]
+        )
+
     def tearDown(self):
-        images_in_logos_directory = os.listdir(self.get_logos_directory())
-        for image in images_in_logos_directory:
-            self.removeImagesCreatedForTests(image)
+        self.tear_down_assistant.remove_logos_created_for_tests()
+
+
+class TestImagesEraser:
+    def remove_logos_created_for_tests(self):
+        logos_directory_files = os.listdir(self.get_logos_directory())
+        for image in logos_directory_files:
+            self.erase(image)
 
     def get_logos_directory(self):
         return os.path.join(
@@ -48,7 +93,7 @@ class TestTechnologyModel(TestCase):
             'technologies_logos'
         )
 
-    def removeImagesCreatedForTests(self, image):
-        logo_img_name_pattern = r"image(?:_\w+)?\.gif"
-        if re.match(logo_img_name_pattern, image):
+    def erase(self, image):
+        regex_pattern_for_test_images = r"image(?:_\w+)?\.gif"
+        if re.match(regex_pattern_for_test_images, image):
             os.remove(os.path.join(self.get_logos_directory(), image))
