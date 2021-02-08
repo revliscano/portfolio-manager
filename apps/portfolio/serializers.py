0443d7b569248874d16c2d33e78f3339565d99e6
revliscano <revliscano@gmail.com>
2021-02-08 01:45:42 -0400

    Cleaned up serializers tests

--- apps/portfolio/serializers.py
+++ apps/portfolio/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from apps.portfolio.models import Project
+from apps.portfolio.models import Project, Technology
 
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
+        fields = '__all__'
+
+
+class TechnologySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Technology
         fields = '__all__'
--- apps/portfolio/tests/test_serializers.py
+++ apps/portfolio/tests/test_serializers.py
@@ -1,16 +1,31 @@
 from django.test import SimpleTestCase
 
-from apps.portfolio.models import Project
-from apps.portfolio.serializers import ProjectSerializer
+from apps.portfolio.models import Project, Technology
+from apps.portfolio.serializers import (
+    ProjectSerializer,
+    TechnologySerializer
+)
 from apps.portfolio.tests.utils import create_object
 
 
-class TestProjectSerializer(SimpleTestCase):
+class TestSerializers(SimpleTestCase):
     def setUp(self):
-        project = create_object(Project, commit=False)
-        self.serializer = ProjectSerializer(project)
+        self.serializers = {
+            Project: ProjectSerializer,
+            Technology: TechnologySerializer
+        }
 
-    def test_contains_all_fields(self):
+    def test_project_serializer_contains_all_fields(self):
         expected_fields = ['id', 'name', 'description', 'year', 'technologies']
-        serializer_fields = self.serializer.data.keys()
+        serializer_fields = self.get_serializer_fields_for(Project)
         self.assertCountEqual(serializer_fields, expected_fields)
+
+    def test_technology_serializer_contains_all_fields(self):
+        expected_fields = ['id', 'name', 'logo']
+        serializer_fields = self.get_serializer_fields_for(Technology)
+        self.assertCountEqual(serializer_fields, expected_fields)
+
+    def get_serializer_fields_for(self, model):
+        object_ = create_object(model, commit=False)
+        serializer = self.serializers[model](object_)
+        return serializer.data.keys()
