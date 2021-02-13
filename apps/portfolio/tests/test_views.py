bebec775fe3ffddf72375728a9b01d12fe80d35f
revliscano <revliscano@gmail.com>
2021-02-13 16:54:19 -0400

    Added ?preview option support for ProjectsList view

--- apps/portfolio/tests/test_views.py
+++ apps/portfolio/tests/test_views.py
@@ -2,7 +2,10 @@ from django.test import TestCase, Client
 from rest_framework import status
 
 from apps.portfolio.models import Project
-from apps.portfolio.serializers import ProjectSerializer
+from apps.portfolio.serializers import (
+    ProjectSerializer,
+    ProjectPreviewSerializer
+)
 from apps.portfolio.tests.utils import create_three_objects_of
 
 
@@ -20,9 +23,49 @@ class TestGetAllProjects(TestCase):
     def setUp(self):
         self.client = Client()
         self.projects = create_three_objects_of(Project)
-        self.serialized_projects = ProjectSerializer(self.projects, many=True)
+        self.target_url = '/api/projects/'
 
     def test_all_projects_are_returned(self):
-        target_url = '/api/projects/'
-        response = self.client.get(target_url)
-        self.assertEqual(response.data, self.serialized_projects.data)
+        serialized_projects = ProjectSerializer(self.projects, many=True)
+        response = self.client.get(self.target_url)
+        self.assertEqual(response.data, serialized_projects.data)
+
+    def test_all_projects_with_preview_queryparam(self):
+        serialized_projects_for_preview = ProjectPreviewSerializer(
+            self.projects,
+            many=True
+        )
+        serialized_projects_full = ProjectSerializer(
+            self.projects,
+            many=True
+        )
+
+        self.performRequests()
+
+        self.assertResponsesWithPreviewReturn(serialized_projects_for_preview)
+        self.assertResponseWithPreviewFalseReturn(serialized_projects_full)
+
+    def performRequests(self):
+        self.response_with_preview = self.client.get(
+            f'{self.target_url}?preview'
+        )
+        self.response_with_preview_true = self.client.get(
+            f'{self.target_url}?preview=true'
+        )
+        self.response_with_preview_false = self.client.get(
+            f'{self.target_url}?preview=false'
+        )
+
+    def assertResponsesWithPreviewReturn(self, serialized_project):
+        responses = (
+            self.response_with_preview, self.response_with_preview_true
+        )
+        assert all(
+            response.data == serialized_project.data
+            for response in responses
+        ), 'Response data should be the same as ProjectPreviewSerializer data'
+
+    def assertResponseWithPreviewFalseReturn(self, serialized_project):
+        response_data = self.response_with_preview_false.data
+        assert response_data == serialized_project.data, \
+            'Response data should be the same as ProjectSerializer data'
--- apps/portfolio/views.py
+++ apps/portfolio/views.py
@@ -1,9 +1,24 @@
 from rest_framework.generics import ListAPIView
 
 from apps.portfolio.models import Project
-from apps.portfolio.serializers import ProjectSerializer
+from apps.portfolio.serializers import (
+    ProjectSerializer,
+    ProjectPreviewSerializer
+)
 
 
 class ProjectsList(ListAPIView):
     queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
+
+    @property
+    def preview_mode(self):
+        return (
+            self.request.query_params.get('preview', 'false') in ('true', '')
+        )
+
+    def get_serializer_class(self):
+        return (
+            ProjectPreviewSerializer
+            if self.preview_mode
+            else ProjectSerializer
+        )
